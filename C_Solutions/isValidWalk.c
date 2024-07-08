20cc4f4e61b583c1c33a1fbf25f78e29e4a353b9
AsherLucRen <82063404+LucasDiabloChan@users.noreply.github.com>
2024-07-08 13:22:18 -0300

    Update isValidWalk.c
    Finished.

--- C_Solutions/isValidWalk.c
+++ C_Solutions/isValidWalk.c
@@ -1,3 +1,11 @@
+  /*
+   - Author: Asher_Ren
+   - Code Wars Kata: Take a Ten Minutes Walk (available in: https://www.codewars.com/kata/54da539698b8a2ad76000228)
+   - Description: given a string, you need to verify if its a valid walk (bring the walker to the start point, in 10 minutes)
+   - Date: 08/07/2024
+  */
+
+#include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 
@@ -8,19 +16,19 @@ bool isValidWalk(const char *walk)
     if(steps != 10)
       return false;
   
-    int inTime = 0;
+    int vertical = 0, horizontal = 0;
       
     for(int i = 0; i < steps; i++)
     {
       switch(walk[i])
       {
-          case 'n': inTime+=1; break;
-          case 'w': inTime+=2; break;
-          case 's': inTime-=1; break;
-          case 'e': inTime-=2; break;
+          case 'n': vertical+=1; break;
+          case 's': vertical-=1; break;
+          case 'w': horizontal+=2; break;
+          case 'e': horizontal-=2; break;
           default: break;
       }
     }
   
-  return (inTime == 0);
+  return (vertical == 0 && horizontal == 0);
 }
